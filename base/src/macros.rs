9278ab1a836dd07fc931006453ad02c0542e4f3f
Lance Hart <shipsimfan@hotmail.com>
2024-07-20 13:53:46 -0400

    Added proc_macro_derive

--- base/src/macros.rs
+++ base/src/macros.rs
@@ -23,3 +23,28 @@ macro_rules! proc_macro_function {
         }
     };
 }
+
+/// A utility for creating procedural macro derives
+///
+/// ## Format
+/// `proc_macro_derive!($name::$type_name)`
+///
+/// ## Parameters
+///  * `$name` - The name of this procedural macro. This will also be the module holding the type
+///              to parse.
+///  * `$type_name` - The name of the type to parse and the trait to derive
+#[macro_export]
+macro_rules! proc_macro_derive {
+    ($(#[$outer: meta])* $name: ident::$type_name: ident) => {
+        mod $namel
+
+        #[proc_macro_derive($type_name)]
+        pub fn $name(input: ::proc_macro::TokenStream) -> ::proc_macro::TokenStream {
+            let token_buffer = input.into();
+            match $crate::parse::<$name::$type_name>(&token_buffer, true) {
+                Ok(output) => $crate::generate(&output),
+                Err(error) => $crate::generate(&error),
+            }
+        }
+    };
+}
