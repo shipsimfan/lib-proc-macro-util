74e1d1d5765ad6820d5b3e9b5da15d969d87161c
Lance Hart <me@lancehart.ca>
2024-09-09 22:39:37 -0400

    Reogranized keyword

---
+++ base/src/tokens/keyword/mod.rs
@@ -0,0 +1,53 @@
+mod r#macro;
+
+r#macro::keywords! [
+    "abstract" Abstract
+    "as" As
+    "async" Async
+    "await" Await
+    "become" Become
+    "box" Box
+    "break" Break
+    "const" Const
+    "continue" Continue
+    "crate" Crate
+    "do" Do
+    "dyn" Dyn
+    "else" Else
+    "enum" Enum
+    "extern" Extern
+    "final" Final
+    "fn" Fn
+    "for" For
+    "if" If
+    "impl" Impl
+    "in" In
+    "let" Let
+    "loop" Loop
+    "macro" Macro
+    "match" Match
+    "mod" Mod
+    "move" Move
+    "mut" Mut
+    "override" Override
+    "priv" Priv
+    "pub" Pub
+    "ref" Ref
+    "return" Return
+    "self" LowerSelf
+    "Self" UpperSelf
+    "static" Static
+    "struct" Struct
+    "super" Super
+    "trait" Trait
+    "try" Try
+    "type" Type
+    "typeof" TypeOf
+    "unsafe" Unsafe
+    "unsized" Unsized
+    "use" Use
+    "virtual" Virtual
+    "where" Where
+    "while" While
+    "yield" Yield
+];
