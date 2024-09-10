b0e22ac763cb00d2f0fbfb7980c3f959ef2244de
Lance Hart <me@lancehart.ca>
2024-09-09 22:11:19 -0400

    Added remaining keywords

--- base/src/tokens/keyword.rs
+++ base/src/tokens/keyword.rs
@@ -14,8 +14,13 @@ macro_rules! keywords {
 
         impl $name {
             #[allow(missing_docs)]
-            pub fn new(span: ::proc_macro::Span) -> Self {
+            pub const fn new(span: ::proc_macro::Span) -> Self {
                 $name { span }
+            }
+
+            #[allow(missing_docs)]
+            pub const fn as_str(&self) -> &'static str {
+                $literal
             }
         }
 
@@ -48,18 +53,53 @@ macro_rules! keywords {
 }
 
 keywords! [
+    "abstract" Abstract
+    "as" As
     "async" Async
     "await" Await
+    "become" Become
+    "box" Box
+    "break" Break
     "const" Const
+    "continue" Continue
     "crate" Crate
+    "do" Do
     "dyn" Dyn
+    "else" Else
+    "enum" Enum
+    "extern" Extern
+    "final" Final
     "fn" Fn
+    "for" For
+    "if" If
+    "impl" Impl
+    "in" In
     "let" Let
+    "loop" Loop
+    "macro" Macro
+    "match" Match
+    "mod" Mod
+    "move" Move
     "mut" Mut
+    "override" Override
+    "priv" Priv
     "pub" Pub
+    "ref" Ref
+    "return" Return
     "self" LowerSelf
     "Self" UpperSelf
+    "static" Static
     "struct" Struct
     "super" Super
+    "trait" Trait
+    "try" Try
+    "type" Type
+    "typeof" TypeOf
     "unsafe" Unsafe
+    "unsized" Unsized
+    "use" Use
+    "virtual" Virtual
+    "where" Where
+    "while" While
+    "yield" Yield
 ];
