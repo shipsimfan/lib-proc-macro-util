33a112627e97b8045a1edd8d594542445e345a45
Lance Hart <me@lancehart.ca>
2024-09-21 14:51:20 -0400

    Updated lib-i18n

--- base/src/ast/expressions/expression/parse.rs
+++ base/src/ast/expressions/expression/parse.rs
@@ -3,6 +3,7 @@ use crate::{
     supported_languages::{EN, FR},
     Parse, Parser, Result,
 };
+use i18n::m;
 
 i18n::message_key!(EXPRESSION_EXPECTED [
     EN => { "expected an expression" },
@@ -15,6 +16,6 @@ impl<'a> Parse<'a> for Expression<'a> {
             return Ok(Expression::Literal(literal));
         }
 
-        Err(parser.error(EXPRESSION_EXPECTED.get(EN.clone()).display(&())))
+        Err(parser.error(m!(EXPRESSION_EXPECTED)))
     }
 }
