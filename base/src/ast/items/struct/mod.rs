fae041ee94db0c12c85f85d3e85e3947329c8cae
Lance Hart <me@lancehart.ca>
2024-11-07 21:21:49 -0500

    Added comments to struct

--- base/src/ast/items/struct/mod.rs
+++ base/src/ast/items/struct/mod.rs
@@ -9,10 +9,18 @@ pub use body::StructBody;
 pub use struct_fields::{StructField, StructFields};
 pub use tuple_fields::{TupleField, TupleFields};
 
+/// An ordered set of child types
 #[derive(Debug, Clone)]
 pub struct Struct<'a> {
+    /// The `struct` token indicating this is a struct
     pub r#struct: Token![struct],
+
+    /// The name of the structure
     pub name: Cow<'a, Identifier>,
+
+    /// Generic parameters affecting the structure
     pub generic_params: Option<GenericParams<'a>>,
+
+    /// The body of the structure
     pub body: StructBody<'a>,
 }
