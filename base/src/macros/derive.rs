b615e2105fa771d3606e344e8fe72f2018f7ad6f
Lance Hart <me@lancehart.ca>
2025-06-21 17:09:56 -0400

    Fixed another bug in derive macro

--- base/src/macros/derive.rs
+++ base/src/macros/derive.rs
@@ -17,7 +17,7 @@ macro_rules! proc_macro_derive {
         $(#[$meta])*
         pub fn $name(input: ::proc_macro::TokenStream) -> ::proc_macro::TokenStream {
             let token_buffer = $crate::collect_token_stream(input);
-            let derive_item = match $crate::parse::<$crate::ast::DeriveItem>(&token_buffer, true) {
+            let derive_item = match $crate::parse::<$crate::ast::DeriveItem>(&token_buffer, true, $crate::Span::call_site().end()) {
                 Ok(derive_item) => derive_item,
                 Err(error) => {
                     error.emit();
